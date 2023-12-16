44844257d6d27fc262883b4f7b6962aa23758971
priyankavaidya-stack <vaidyapriyanka80@gmail.com>
2023-12-16 16:50:47 +0530

    typesrcipt setup initiation completed

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// console.log("Hello World");
+// as it is javascript code , it will create js file with that js code included into it.
+
+let age: number = 20;
+    if( age < 50)
+        age += 10;
+console.log(age);
